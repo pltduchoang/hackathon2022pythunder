aa75c051da40c4fd44f169fafdb20ab6359e89cc
pltduchoang <plt.duchoang@gmail.com>
2022-12-15 11:35:08 -0700

    update

--- mainapp.py
+++ mainapp.py
@@ -2,6 +2,7 @@ import class_pythunder as c
 import os
 
 object_list_of_data = []
+list_of_function_label = ['Exit','View stat by commnutiy','View stat by year','View stat by percentate over population','View stat by crime category']
 
 here = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(here, "data.csv")
@@ -16,10 +17,35 @@ def print_object():
     for object in object_list_of_data:
         print(object)
 
-def create_year_list():
-    pass
+def main_menu():
+    list_of_function = [0,communitiy_data_by_year]
+    while True:
+        print(f'Welcome to City of Calgary crime stat program')
+        for i in range(len(list_of_function)):
+            print(f'{i}  -   {list_of_function[i]}')
+        user_choice = inputRangeCheck(input('Enter your option'))
+        if user_choice == 0:
+            break
+        else:
+            list_of_function[i]()
+    
+    
+def inputRangeCheck(x):
+    range_check = False
+    while not range_check:
+        if x.isdigit():
+            if not int(x) in range (0,len()):
+                x = input('Invalid, input not in range, try again: ')
+            else:
+                range_check = True
+        else: 
+            x = input('Invalid input, please enter number only again: ')
+    return x
 
-def function_name_2():
+
+
+
+def communitiy_data_by_year():
     pass
 
 def function_name_3():
@@ -31,4 +57,4 @@ def function_name_4():
 def function_name_5():
     pass
 
-print_object()
+# print_object()
