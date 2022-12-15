3592de4c37962e027988a59a6078fbee107ccc97
pltduchoang <97260149+pltduchoang@users.noreply.github.com>
2022-12-14 20:39:50 -0700

    update

--- mainapp.py
+++ mainapp.py
@@ -1,31 +1,47 @@
+# import class_of_anything as c
+# import os
+
+# object_list = []
+
+# here = os.path.dirname(os.path.abspath(__file__))
+# filename = os.path.join(here, "DATA_FILE_NAME_HERE")
+# file_data = open(filename, "r")
+# for line in file_data:
+#     line_data = line.lstrip().rstrip().split('REPLACE_BY_ANY_THING')
+#     object_list.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6],line_data[8]))
+
+
+# def print_object():
+#     for object in object_list:
+#         print(object)
+
+# def function_name_1():
+#     pass
+
+# def function_name_1():
+#     pass
+
+# def function_name_1():
+#     pass
+
+# def function_name_1():
+#     pass
+
+# def function_name_1():
+#     pass
+
+
 import class_of_anything as c
 import os
 
-object_list = []
+object_list_of_cases = []
 
 here = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(here, "DATA_FILE_NAME_HERE")
+filename = os.path.join(here, "practice.txt")
 file_data = open(filename, "r")
+file_data.readline()
 for line in file_data:
-    line_data = line.lstrip().rstrip().split('REPLACE_BY_ANY_THING')
-    object_list.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6],line_data[8]))
+    line_data = line.lstrip().rstrip().split(' ')
+    object_list_of_cases.append(c.pythunder(line_data[1],line_data[2],line_data[3],line_data[4]))
 
 
-def print_object():
-    for object in object_list:
-        print(object)
-
-def function_name_1():
-    pass
-
-def function_name_1():
-    pass
-
-def function_name_1():
-    pass
-
-def function_name_1():
-    pass
-
-def function_name_1():
-    pass
