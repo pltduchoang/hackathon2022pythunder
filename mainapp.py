65151eca32a03d852e6216d3b363c57c0590a642
pltduchoang <plt.duchoang@gmail.com>
2022-12-15 11:08:41 -0700

    update

--- mainapp.py
+++ mainapp.py
@@ -6,6 +6,7 @@ object_list_of_data = []
 here = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(here, "data.csv")
 file_data = open(filename, "r")
+file_data.readline()
 for line in file_data:
     line_data = line.lstrip().rstrip().split(',')
     object_list_of_data.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6]))
@@ -15,7 +16,7 @@ def print_object():
     for object in object_list_of_data:
         print(object)
 
-def function_name_1():
+def create_year_list():
     pass
 
 def function_name_2():
