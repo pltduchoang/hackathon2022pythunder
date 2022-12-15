74aeafdd7f122f330fe034749b5812d5df19616f
pltduchoang <plt.duchoang@gmail.com>
2022-12-15 10:43:40 -0700

    update class

--- class_pythunder.py
+++ class_pythunder.py
@@ -1,17 +1,17 @@
 class pythunder:
-    def __init__(self, DATA0, DATA1, DATA2, DATA3, DATA4, DATA5, DATA6, DATA7):
-        self.DATA0 = DATA0
-        self.DATA1 = DATA1
-        self.DATA2 = DATA2
-        self.DATA3 = DATA3
-        self.DATA4 = DATA4
-        self.DATA5 = DATA5
-        self.DATA6 = DATA6
-        self.DATA7 = DATA7
+    def __init__(self, sector, community, crimecat, crimecount, residentpop, date, year):
+        self.sector = sector
+        self.community = community
+        self.crimecat = crimecat
+        self.crimecount = crimecount
+        self.residentpop = residentpop
+        self.date = date
+        self.year = year
+
 
 
     def __str__(self):
-        result = f'{self.DATA0}{self.DATA1}{self.DATA2}{self.DATA3}{self.DATA4}{self.DATA5}{self.DATA6}{self.DATA7}'
+        result = f'{self.sector}{self.community}{self.crimecat}{self.crimecount}{self.residentpop}{self.date}{self.year}'
         return result
 
 
