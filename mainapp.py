ec5a8d486d4cb09d0cc9a49aeeadfa1534f524a5
kngjms15 <118232516+kngjms15@users.noreply.github.com>
2022-12-15 12:26:51 -0700

    dsfg

--- mainapp.py
+++ mainapp.py
@@ -8,7 +8,7 @@ filename = os.path.join(here, "data.csv")
 file_data = open(filename, "r")
 for line in file_data:
     line_data = line.lstrip().rstrip().split(',')
-    object_list_of_data.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6]))
+    object_list_of_data.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6], line_data[7]))
 
 
 def print_object():
