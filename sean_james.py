afb5cfa9ba85a3a6588b6ac1f45feaff03178df4
kngjms15 <118232516+kngjms15@users.noreply.github.com>
2022-12-15 13:25:31 -0700

    duc's changes

--- sean_james.py
+++ sean_james.py
@@ -15,17 +15,66 @@ grand_list = m.object_list_of_data
 #         if search == com_sort.community:
 #             print(com_sort)
 
-def crimeCount():
-    pass
-
-def pop():
-    pass
-
-list_of_community = []
+list_of_sector = []
 for object in grand_list:
-    if object.community not in list_of_community:
-        list_of_community.append(object.community)
+    if object.sector not in list_of_sector:
+        list_of_sector.append(object.sector)
+del(list_of_sector[-1])
 
-print(list_of_community)
+def menu():
+    for i in range(len(list_of_sector)):
+        print(f'{i+1}  -  {list_of_sector[i]}') 
+    print('Enter 0 to back to main menu')
+    user_choice=input('Enter sector')
+    viewBySector(list_of_sector[int(user_choice)-1])
 
+def viewBySector(x):
+    list_of_community = []
+    for object in grand_list:
+        if object.sector == x and object.community not in list_of_community:
+            list_of_community.append(object.community)
+    list_of_crime_count = []
+    
+    
+    for community in list_of_community:
+        crime_counter = 0
+
+        for object in grand_list:
+            if object.community == community:
+                crime_counter += int(object.crimecount)
+                
+        list_of_crime_count.append(crime_counter)
+
+    
+    
+    
+    percentage = 0    
+    for z in range(len(list_of_community)):
+        
+        # percentage = int(list_of_crime_count[z])/int(population_list[z]) 
+        print(f'{"Name of community":<25}{"Crime from 2017":<15}')
+        print(f'{list_of_community[z]:<25} {list_of_crime_count[z]:<15}')
+
+    
+    while True:
+        user_find = input("Enter specific community name: ").upper()
+        if user_find != 0:
+            x=0
+            while x in range (len(list_of_community)+1):
+                if x < len(list_of_community) and list_of_community[x] == user_find:
+                    print(f'{"Name of community: ":<25}{"Crime from 2017":<15}')
+                    print(f'{list_of_community[x]:<25} {list_of_crime_count[x]:<15}')
+                    break
+                if x == len(list_of_community):
+                    print('Communnity Name not found!')
+                else:
+                    x += 1
+                
+        if user_find == 0:
+
+            break
+    menu()        
+    
 # searchCommunity(
+print(list_of_sector)
+menu()
