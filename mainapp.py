182cf7f460f25f4b6413ee0ae91c1c9e73b55a97
pltduchoang <97260149+pltduchoang@users.noreply.github.com>
2022-12-14 21:50:45 -0700

    update

--- mainapp.py
+++ mainapp.py
@@ -1,80 +1,32 @@
-# import class_of_anything as c
-# import os
-
-# object_list = []
-
-# here = os.path.dirname(os.path.abspath(__file__))
-# filename = os.path.join(here, "DATA_FILE_NAME_HERE")
-# file_data = open(filename, "r")
-# for line in file_data:
-#     line_data = line.lstrip().rstrip().split('REPLACE_BY_ANY_THING')
-#     object_list.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6],line_data[8]))
-
-
-# def print_object():
-#     for object in object_list:
-#         print(object)
-
-# def function_name_1():
-#     pass
-
-# def function_name_1():
-#     pass
-
-# def function_name_1():
-#     pass
-
-# def function_name_1():
-#     pass
-
-# def function_name_1():
-#     pass
-
-
 import class_of_anything as c
 import os
 
-object_list_of_cases = []
+object_list = []
 
 here = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(here, "practice.csv")
+filename = os.path.join(here, "DATA_FILE_NAME_HERE")
 file_data = open(filename, "r")
-file_data.readline() #skip the header
 for line in file_data:
-    line_data = line.lstrip().rstrip().split(',')
-    object_list_of_cases.append(c.pythunder(line_data[1],line_data[2],line_data[3],line_data[4]))
-
-category_list = []
-
-for object in object_list_of_cases:
-    if object.cat not in category_list:
-        category_list.append(object.cat)
-
-print (category_list)
+    line_data = line.lstrip().rstrip().split('REPLACE_BY_ANY_THING')
+    object_list.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6],line_data[8]))
 
 
-community_list = []
+def print_object():
+    for object in object_list:
+        print(object)
 
-for object in object_list_of_cases:
-    if object.community not in community_list:
-        community_list.append(object.community)
+def function_name_1():
+    pass
 
-crime_count_list = []
+def function_name_1():
+    pass
 
-for ech_community in community_list:
-    crime_count = 0
-    for object in object_list_of_cases:
-        if object.community == ech_community:
-            crime_count += int(object.count)
-    crime_count_list.append(crime_count)
+def function_name_1():
+    pass
 
-print(community_list)
+def function_name_1():
+    pass
 
-print(crime_count_list)
+def function_name_1():
+    pass
 
-for x in range (len(community_list)):
-    print (community_list[x])
-    print (crime_count_list[x])
-
-crime_count_list.sort()
-print(crime_count_list[-1])
