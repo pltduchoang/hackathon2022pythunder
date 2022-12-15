9b46bb629dd7a700862d56942443fa776bd35711
kngjms15 <118232516+kngjms15@users.noreply.github.com>
2022-12-15 14:03:09 -0700

    nimor change

--- sean_james.py
+++ sean_james.py
@@ -1,19 +1,7 @@
 import class_pythunder as c
 import mainapp as m
 
-
 grand_list = m.object_list_of_data
-
-# def searchCommunity():
-#     comm_names = []
-#     search = input("Enter Community: ")
-#     j = 0
-#     for k in grand_list:
-#         com_sort = c.Community(grand_list[j].DATA1, grand_list[j].DATA3, grand_list[j].DATA4)
-#         comm_names.append(com_sort)
-#         j += 1   
-#         if search == com_sort.community:
-#             print(com_sort)
 
 list_of_sector = []
 for object in grand_list:
@@ -21,7 +9,14 @@ for object in grand_list:
         list_of_sector.append(object.sector)
 del(list_of_sector[-1])
 
+
 def menu():
+    # list_of_sector = []
+    # for object in grand_list:
+    #     if object.sector not in list_of_sector:
+    #         list_of_sector.append(object.sector)
+    # del(list_of_sector[-1])
+
     for i in range(len(list_of_sector)):
         print(f'{i+1}  -  {list_of_sector[i]}') 
     print('Enter 0 to back to main menu')
@@ -61,7 +56,7 @@ def viewBySector(x):
         if list_of_population[z] != 0 and list_of_population[z] != "" and list_of_population[z] != '0':
             percentage = (int(list_of_crime_count[z])/int(list_of_population[z])) * 100
         print(f'{"Name of community":<25}{"Crime from 2017":<15}{"Percentage":10}')
-        print(f'{list_of_community[z]:<25} {list_of_crime_count[z]:<15} {percentage:10}%')
+        print(f'{list_of_community[z]:<25} {list_of_crime_count[z]:<15} {round(percentage, 2):10}%')
 
     
 
@@ -83,6 +78,4 @@ def viewBySector(x):
             break
     menu()        
 
-# searchCommunity(
-print(list_of_sector)
 menu()
