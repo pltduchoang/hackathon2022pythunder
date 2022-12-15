a95090a19d836bfaadf9c722c20bc3f5942fcdd4
pltduchoang <plt.duchoang@gmail.com>
2022-12-15 10:43:29 -0700

    update class

--- mainapp.py
+++ mainapp.py
@@ -1,13 +1,13 @@
-import class_of_anything as c
+import class_pythunder as c
 import os
 
 object_list = []
 
 here = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(here, "DATA_FILE_NAME_HERE")
+filename = os.path.join(here, "data.csv")
 file_data = open(filename, "r")
 for line in file_data:
-    line_data = line.lstrip().rstrip().split('REPLACE_BY_ANY_THING')
+    line_data = line.lstrip().rstrip().split(',')
     object_list.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6],line_data[8]))
 
 
