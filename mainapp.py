284ea66f5339d21949fd36844066b8929dd3de17
pltduchoang <plt.duchoang@gmail.com>
2022-12-15 10:45:13 -0700

    updatemmain

--- mainapp.py
+++ mainapp.py
@@ -1,18 +1,18 @@
 import class_pythunder as c
 import os
 
-object_list = []
+object_list_of_data = []
 
 here = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(here, "data.csv")
 file_data = open(filename, "r")
 for line in file_data:
     line_data = line.lstrip().rstrip().split(',')
-    object_list.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6],line_data[8]))
+    object_list_of_data.append(c.pythunder(line_data[0],line_data[1],line_data[2],line_data[3],line_data[4],line_data[5],line_data[6]))
 
 
 def print_object():
-    for object in object_list:
+    for object in object_list_of_data:
         print(object)
 
 def function_name_1():
